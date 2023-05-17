7911082a7f777bc8b8bd18549608c35674d498b5
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2023-05-17 05:34:49 +0200

    Fix dates and empty tags, add CV

--- src/_plugins/TranslatedDate.rb
+++ src/_plugins/TranslatedDate.rb
@@ -6,38 +6,37 @@ module Jekyll
     @@lang = nil
     @@default_format = nil
 
-    def translate_date(date_value, lang, format)
+    def translated_date(parsed_date, lang, format)
       unless @@lang
-        @@lang ||= @context.registers[:site]['lang']
+        @@lang ||= @context.registers[:site].config['lang']
         @@lang ||= 'en'
       end
       lang ||= @@lang
 
       unless @@default_format
-        @@default_format ||= @context.registers[:site]['date_format']
+        @@default_format ||= @context.registers[:site].config['date_format']
         @@default_format ||= '%b %-d, %Y'
       end
-      format ||= @default_format
+      format ||= @@default_format
 
-      @locales ||= @context.registers[:site]['locales']
-
-      parsed_date = DateTime.strptime(date_value)
+      @locales ||= @context.registers[:site].data['locales']
+      locale = @locales[lang]
 
       num_day = parsed_date.strftime('%w').to_i
       num_month = parsed_date.strftime('%-m').to_i - 1
 
       translated_date_format = format
-      if locales[lang]&.abbreviated_weekday[num_day]
-        translated_date_format = translated_date_format.replace('%a', locales[lang].abbreviated_weekday[num_day])
+      if locale['abbreviated_weekday'] && locale['abbreviated_weekday'][num_day]
+        translated_date_format.gsub! '%a', locale['abbreviated_weekday'][num_day]
       end
-      if locales[lang]&.full_weekday[num_day]
-        translated_date_format = translated_date_format.replace('%A', locales[lang].full_weekday[num_day])
+      if locale['full_weekday'] && locale['full_weekday'][num_day]
+        translated_date_format.gsub! '%A', locale['full_weekday'][num_day]
       end
-      if locales[lang]&.abbreviated_month[num_month]
-        translated_date_format = translated_date_format.replace('%b', locales[lang].abbreviated_month[num_month])
+      if locale['abbreviated_month'] && locale['abbreviated_month'][num_month]
+        translated_date_format.gsub! '%b', locale['abbreviated_month'][num_month]
       end
-      if locales[lang]&.full_month[num_month]
-        translated_date_format = translated_date_format.replace('%B', locales[lang].full_month[num_month])
+      if locale['full_month'] && locale['full_month'][num_month]
+        translated_date_format.gsub! '%B', locale['full_month'][num_month]
       end
 
       parsed_date.strftime(translated_date_format)
